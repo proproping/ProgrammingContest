219e3a04bbd735cea81c08b5fecd7b9e863cd237
proproping <hlux2xrr5su7h@outlook.jp>
2020-01-25 20:33:50 +0900

    cr

--- Codeforces/CR/CR615/b.py
+++ Codeforces/CR/CR615/b.py
@@ -1,5 +1,29 @@
 def main():
-    
+    from operator import itemgetter
+    t = int(input())
+    for i in range(t):
+        flag = False
+        n = int(input())
+        xy = [list(map(int,input().split())) for _ in range(n)]
+        xy = sorted(xy,key=itemgetter(0))
+        xy = sorted(xy,key=itemgetter(1))
+        x,y = 0,0
+        ans = ""
+        for j in xy:
+            if j[0]-x < 0 or j[1]-y < 0:
+                x = j[0]
+                y = j[1]
+                flag = True
+            else:
+                ans += "R"*(j[0]-x)+"U"*(j[1]-y)
+                x = j[0]
+                y = j[1]
+        if flag:
+            print("NO")
+        else:
+            print("YES")
+            print(ans)
+
 
 if __name__ == '__main__':
     main()
--- Codeforces/CR/CR615/c.py
+++ Codeforces/CR/CR615/c.py
@@ -1,5 +1,43 @@
+import math
+def isPrime(n):
+    i = 2
+    while i**2 <= n:
+        if n%i == 0:
+            return False
+        i += 1
+    return n != 1
 def main():
-    
+    t = int(input())
+    for i in range(t):
+        a,b,c = 0,0,0
+        n = int(input())
+        if isPrime(n):
+            print("NO")
+        else:
+            flag = False
+            for j in range(2,math.ceil(n/2)+1):
+                if flag:
+                    break
+                if n/j == n//j:
+                    if not isPrime(n//j):
+                        a = j
+                        b = n//j
+                        flag = True
+            flag = False
+            for k in range(2,math.ceil(b/2)+1):
+                if flag:
+                    break
+                if b/k == b//k:
+                    if not isPrime(b//k):
+                        b = b//k
+                        c = k
+                        flag = True
+            if a == 0 or b == 0 or c == 0:
+                print("NO")
+            else:
+                print("YES")
+                print(a,b,c)
+
 
 if __name__ == '__main__':
     main()
