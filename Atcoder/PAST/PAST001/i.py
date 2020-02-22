a7806bd2b75d11e1b99255112cd2dad699b69eb0
proproping <hlux2xrr5su7h@outlook.jp>
2020-02-22 16:11:14 +0900

    atc

--- Atcoder/PAST/PAST001/i.py
+++ Atcoder/PAST/PAST001/i.py
@@ -1,10 +1,22 @@
-import numpy as np
-
 def main():
+    INF = float('inf')
     N,M = map(int,input().split())
-    sc = [input().split() for _ in range(M)]
-    
-
+    s,c = [],[]
+    for i in range(M):
+        t1,t2 = input().split()
+        t1 = t1.replace('Y','1').replace('N','0')
+        s.append(int(t1,2))
+        c.append(int(t2))
+    dp = [[INF]*(2**N) for _ in range(M+1)]
+    dp[0][0] = 0
+    for i in range(M):
+        for j in range(2**N):
+            dp[i+1][j|s[i]] = min(dp[i+1][j|s[i]],dp[i][j] + c[i])
+            dp[i+1][j] = min(dp[i+1][j],dp[i][j])
+    if dp[M][2**N-1] == INF:
+        print(-1)
+    else:
+        print(dp[M][2**N-1])
 
 if __name__ == '__main__':
     main()
--- Atcoder/PAST/PAST001/j.py
+++ Atcoder/PAST/PAST001/j.py
@@ -1,5 +1,23 @@
 def main():
-    
+    H,W = map(int,input().split())
+    v = [[] for _ in range(H*W)]
+    A = [list(map(int,input().split())) for _ in range(H)]
+    dx = [-1,-1,0]
+    dy = [1,0,1]
+    for i in range(H):
+        for j in range(W):
+            for k in range(3):
+                if 0 <= i-dy[k] and 0 <= j+dx[k]:
+                    if k == 0:
+                        ti,tj = -1,-1
+                    elif k == 1:
+                        ti,tj = -1,0
+                    else:
+                        ti,tj = 1,-1
+                    v[i*W+j].append([i*W+j,A[i-dy[k]][i+dx[k]]])
+    print(v)
+
+
 
 if __name__ == '__main__':
     main()
