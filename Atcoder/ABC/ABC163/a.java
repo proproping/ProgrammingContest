535a9e5fa956123914eb4dd66e377abe1c4895a7
proproping <hlux2xrr5su7h@outlook.jp>
2020-04-20 22:42:54 +0900

    abc

---
+++ Atcoder/ABC/ABC163/a.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double R = input.nextInt();
+        double ans = 2*R*Math.PI;
+        System.out.println(ans);
+    }
+}
