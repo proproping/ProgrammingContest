1e300902fcf7576c4ab2fda00e427f6d8a2a4af3
proproping <hlux2xrr5su7h@outlook.jp>
2020-05-03 17:25:29 +0900

    abc

--- Atcoder/ABC/ABC165/c.py
+++ Atcoder/ABC/ABC165/c.py
@@ -1,14 +1,35 @@
+import copy
+
+arrays = []
+n,m,q = 0,0,0
+
 def main():
+    global n, m, q
+    global arrays
     n, m, q = map(int, input().split())
-    integers = [list(map(int, input().split())) for _ in range(q)]
+    abcd = [list(map(int, input().split())) for _ in range(q)]
     ans = 0
-    arrays = []
-    tmp = [0]*n
+    dfs([], 1)
+    for array in arrays:
+        tmp = 0
+        for i in range(q):
+            if array[abcd[i][1]-1] - array[abcd[i][0]-1] == abcd[i][2]:
+                tmp += abcd[i][3]
+        ans = max(ans, tmp)
+    print(ans)
 
-def dfs(m):
-
-
-
+def dfs(stack, i):
+    stack.append(i)
+    if len(stack) == n:
+        arrays.append(copy.deepcopy(stack))
+        if i < m:
+            stack.pop()
+            dfs(copy.deepcopy(stack), i + 1)
+        return
+    dfs(copy.deepcopy(stack), i)
+    if i < m:
+        stack.pop()
+        dfs(copy.deepcopy(stack), i + 1)
 
 if __name__ == '__main__':
     main()
