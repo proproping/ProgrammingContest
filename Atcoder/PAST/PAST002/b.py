0da9cbd5157fa34d8b13d0a94bec22ba6d4e8e06
proproping <hlux2xrr5su7h@outlook.jp>
2020-05-10 09:09:08 +0900

    past

--- Atcoder/PAST/PAST002/b.py
+++ Atcoder/PAST/PAST002/b.py
@@ -1,5 +1,14 @@
+from collections import Counter
+
 def main():
-    
+    s = list(input())
+    c = Counter(s)
+    maxvote = max(c.values())
+    ans = ""
+    for key in c.keys():
+        if c[key] == maxvote:
+            ans = key
+    print(ans)
 
 if __name__ == '__main__':
     main()
