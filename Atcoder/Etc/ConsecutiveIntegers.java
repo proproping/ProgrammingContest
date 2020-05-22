1d24e892977e48511648534eef97254484fd105d
proproping <hlux2xrr5su7h@outlook.jp>
2020-05-22 18:31:21 +0900

    other

---
+++ Atcoder/Etc/ConsecutiveIntegers.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+        System.out.println(n-k+1);;
+    }
+}
