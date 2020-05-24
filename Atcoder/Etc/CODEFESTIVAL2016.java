d8919c96abb98d98a5aebb7881a515f9ada27bb1
proproping <hlux2xrr5su7h@outlook.jp>
2020-05-24 10:14:44 +0900

    other

---
+++ Atcoder/Etc/CODEFESTIVAL2016.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        System.out.println(s.substring(0,4)+" "+s.substring(4,12));
+    }
+}
