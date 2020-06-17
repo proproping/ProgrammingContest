f5c2f3ca5effa9856f335ac3150c6c267d71928a
proproping <hlux2xrr5su7h@outlook.jp>
2020-06-18 07:58:41 +0900

    etc

---
+++ Atcoder/ABC/ABC170/c.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int n = input.nextInt();
+        if (n == 0){
+            System.out.println(x);
+        } else {
+            
+        }
+    }
+}
---
+++ Atcoder/Etc/ダブル文字列.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        System.out.println(s+s);
+    }
+}
+
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         String s = input.next();
+//         String ans = s.substring(0,s.length()-4);
+//         System.out.println(ans+"2015");
+//     }
+// }
