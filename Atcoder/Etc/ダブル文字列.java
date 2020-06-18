463bf990abf14c65f272241dad1eae3b8e0bbc5b
proproping <hlux2xrr5su7h@outlook.jp>
2020-06-18 09:52:58 +0900

    etc

--- Atcoder/Etc/ダブル文字列.java
+++ Atcoder/Etc/ダブル文字列.java
@@ -6,13 +6,3 @@ public class Main {
         System.out.println(s+s);
     }
 }
-
-// import java.util.*;
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         String s = input.next();
-//         String ans = s.substring(0,s.length()-4);
-//         System.out.println(ans+"2015");
-//     }
-// }
