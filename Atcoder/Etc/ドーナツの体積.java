6f7eb9f6393f569d74e480bb592301a1d0453a8c
proproping <hlux2xrr5su7h@outlook.jp>
2020-06-20 10:07:06 +0900

    etc

---
+++ Atcoder/Etc/ドーナツの体積.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double r = input.nextDouble();
+        double d = input.nextDouble();
+        System.out.println(2*d*Math.PI * r*r*Math.PI);
+    }
+}
