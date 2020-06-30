9a3f8c98ff00cd2674a8d02e2d8af771cc0530dd
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-01 07:03:14 +0900

    etc

---
+++ Atcoder/Etc/サイコロ.java
@@ -0,0 +1,9 @@
+// サイコロ
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println(7-a);
+    }
+}
--- Atcoder/Etc/ストック.java
+++ Atcoder/Etc/ストック.java
@@ -1,11 +1,40 @@
-帰ってきた器物破損！高橋君
-import java.util.*;
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String x = input.next();
-        String s = input.next();
-        s = s.replaceAll(x,"");
-        System.out.println(s);
-    }
-}
+// // 整数割り算
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int n = input.nextInt();
+//         for (int i = 0; i < n; i++){
+//             int a = input.nextInt();
+//             int b = input.nextInt();
+//             System.out.println(a/b);
+//         }
+//     }
+// }
+
+// // 立方数
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         int a = input.nextInt();
+//         String ans = "NO";
+//         for (int i = 0; i < 101; i++){
+//             if (i*i*i == a){
+//                 ans = "YES";
+//             }
+//         }
+//         System.out.println(ans);
+//     }
+// }
+
+// // HAGIXILE
+// import java.util.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         String s = input.next();
+//         s = s.replaceAll("HAGIYA","HAGIXILE");
+//         System.out.println(s);
+//     }
+// }
