599e806f8ba865beed07a1af19ca1f8afc3faf21
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-02 07:01:28 +0900

    etc

---
+++ Atcoder/Etc/HAGIXILE.java
@@ -0,0 +1,10 @@
+// HAGIXILE
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        s = s.replaceAll("HAGIYA","HAGIXILE");
+        System.out.println(s);
+    }
+}
--- Atcoder/Etc/ストック.java
+++ Atcoder/Etc/ストック.java
@@ -27,14 +27,3 @@
 //         System.out.println(ans);
 //     }
 // }
-
-// // HAGIXILE
-// import java.util.*;
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         String s = input.next();
-//         s = s.replaceAll("HAGIYA","HAGIXILE");
-//         System.out.println(s);
-//     }
-// }
