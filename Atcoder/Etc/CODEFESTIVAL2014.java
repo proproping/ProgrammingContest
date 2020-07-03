aed9798993ebb7f32345937c5386baa9d0ffae21
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-04 00:33:16 +0900

    etc

---
+++ Atcoder/Etc/CODEFESTIVAL2014.java
@@ -0,0 +1,9 @@
+// CODEFESTIVAL2014
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s = input.next();
+        System.out.println(s+2014);
+    }
+}
--- Atcoder/Etc/ストック.java
+++ Atcoder/Etc/ストック.java
@@ -1,12 +1,4 @@
-// // CODEFESTIVAL2014
-// import java.util.*;
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         String s = input.next();
-//         System.out.println(s+2014);
-//     }
-// }
+
 
 // // CodeFormula2015
 // import java.util.*;
