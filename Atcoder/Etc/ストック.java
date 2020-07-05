d3c1f9fb661d69717a6103ee54e813ece17ee8d7
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-05 21:27:16 +0900

    etc

--- Atcoder/Etc/ストック.java
+++ Atcoder/Etc/ストック.java
@@ -1,14 +1,3 @@
-// // あるピアニスト
-// import java.util.*;
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         int a = input.nextInt();
-//         int b = input.nextInt();
-//         System.out.println(Math.max(a,b));
-//     }
-// }
-
 // // 50m走
 // import java.util.*;
 // public class Main {
