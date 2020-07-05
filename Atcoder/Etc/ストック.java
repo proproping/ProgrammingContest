3696bc1e6fb9d5935aafd6d8768798f22e614c3b
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-06 07:00:38 +0900

    etc

--- Atcoder/Etc/ストック.java
+++ Atcoder/Etc/ストック.java
@@ -18,14 +18,3 @@
 //         System.out.println(a*4 + b*2);
 //     }
 // }
-
-// // 朝食
-// import java.util.*;
-// public class Main {
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         int a = input.nextInt();
-//         int b = input.nextInt();
-//         System.out.println(Math.max(a,b));
-//     }
-// }
