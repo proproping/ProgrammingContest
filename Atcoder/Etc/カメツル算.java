3fd2caa56b51ee12882fbbe065539216aacdc6ee
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-08 06:30:58 +0900

    etc

---
+++ Atcoder/Etc/カメツル算.java
@@ -0,0 +1,10 @@
+// カメツル算
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        System.out.println(a*4 + b*2);
+    }
+}
