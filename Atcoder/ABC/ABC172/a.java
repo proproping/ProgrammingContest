51090d45a494da34a5156ef85bc7c555d9aa5be5
proproping <hlux2xrr5su7h@outlook.jp>
2020-07-09 07:36:37 +0900

    abc

---
+++ Atcoder/ABC/ABC172/a.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        System.out.println(a+a*a+a*a*a);
+    }
+}
